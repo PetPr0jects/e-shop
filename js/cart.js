24fd7c2a8d657db577af7d5a998e53a52156b201
PetPr0jects <practiceofpet@gmail.com>
2022-06-19 15:32:31 +0600

    Made the delete from cart button. And added some stylings on its mobile design.

--- js/cart.js
+++ js/cart.js
@@ -6,13 +6,15 @@
 //         $(this).css("display", "none");
 //     });
 // });
-
+const itemPrice = document.querySelectorAll(".itemPrice");
+const totalPrice = document.querySelector(".customerTotalPrice");
 const plusBtn = document.querySelectorAll(".plusBtn");
 plusBtn.forEach((element, index) => element.addEventListener('click', () => {
     const itemCounter = document.querySelectorAll(".itemCounter");
     let itemCount = itemCounter[index].textContent;
     let itemCountInt = parseInt(itemCount)+1;
     itemCounter[index].textContent = itemCountInt;
+    totalPrice.textContent = itemCountInt*parseFloat(itemPrice[index].textContent);
 }));
 const minusBtn = document.querySelectorAll(".minusBtn");
 minusBtn.forEach((element, index) => element.addEventListener('click', () => {
@@ -26,8 +28,15 @@ minusBtn.forEach((element, index) => element.addEventListener('click', () => {
     itemCounter[index].textContent = itemCountInt;
     }
 }));
-const items = document.querySelectorAll(".item");
+const emptyCart = document.querySelector(".emptyCart");
+const items = Array.from(document.querySelectorAll(".item-li"));
 const deleteBtn = document.querySelectorAll(".deleteBtn");
 deleteBtn.forEach((element, index) => element.addEventListener('click', () => {
     items[index].style.display = 'none';
+        const filteredArray = items.filter((el) => el.style.display == 'none');
+    if(filteredArray.length == items.length){
+        const total = document.querySelector(".total");
+        total.style.display = "none";
+        emptyCart.style.display = "flex";
+    }
 }));
